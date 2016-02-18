1e07fde9b2a1c1f143d291a76d46c8ae678e2237
genelim <wernminlim@gmail.com>
2016-02-18 17:18:11 +0800

    Fixed modal bug

--- public/app/admin/user.js
+++ public/app/admin/user.js
@@ -5,5 +5,9 @@ angular
 AdminUserController.$inject = [];
 
 function AdminUserController(){ 
-    
+    var vm = this;
+    vm.open_modal = open_modal;
+    function open_modal(){
+        $('#add_user').openModal();
+    }
 }
